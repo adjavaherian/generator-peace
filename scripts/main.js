4589b3dd8248258ece67beefae077f11939e2a90
Amir Djavaherian <a_mir@icloud.com>
2014-10-03 11:54:50 -0700

    Renders box.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,39 @@
 //main.js
 'use strict';
 
-console.log('Peace, Love and Pixels.');
+window.onload = function(){
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    console.log('Peace, Love and Pixels.');
 
-var renderer = new THREE.WebGLRenderer();
+    var stage = document.getElementById('stage');
+    var sW = $('#stage').css('width').substring(0,3) - 2;
+    var sH = $('#stage').css('height').substring(0,3) - 2;
 
-var stage = document.getElementsByClassName('stage')[0];
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, sW/sH, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer();
 
-//    renderer.setSize( 480, 200);
-//    stage.appendChild( renderer.domElement );
+   renderer.setSize(sW, sH);
+
+    var geometry = new THREE.BoxGeometry(1,1,1);
+    var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    var render = function () {
+        requestAnimationFrame(render);
+
+        cube.rotation.x += 0.1;
+        cube.rotation.y += 0.1;
+
+        renderer.render(scene, camera);
+    };
+
+    render();
+
+    stage.appendChild(renderer.domElement);
+
+};
